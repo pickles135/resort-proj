a34cdce1a597dfc688207bc631b18d9c7034bc7c
Teresa Cho <tcho@cca.edu>
2020-07-02 15:34:25 -0700

    imports Link into our Navbar.js file to help with direct linking for us

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import {FaAlignRight} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
@@ -10,4 +11,4 @@ export default class Navbar extends Component {
         </div>
       )
     }
-  };  
+  };   
