a28a986b0ef7a04cd185a496d9983c409403c322
Teresa Cho <tcho@cca.edu>
2020-07-01 17:23:19 -0700

    imports the error into App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
-
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
 
 function App() {
   return (
