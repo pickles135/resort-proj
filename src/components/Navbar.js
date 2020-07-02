0fc6a7734accbca8d352970f2f9f18b4cb895eac
Teresa Cho <tcho@cca.edu>
2020-07-02 15:32:00 -0700

    fixes import statement for react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
-import {FaAlignRight} from 'react-icons';
+import {FaAlignRight} from 'react-icons/fa';
 
 export default class Navbar extends Component {
     render() {
