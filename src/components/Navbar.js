2fbc7166365eededa103467d2861525770c80259
Teresa Cho <tcho@cca.edu>
2020-07-02 17:43:16 -0700

    passes handleToggle into an onClick handler in button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,11 @@ export default class Navbar extends Component {
               <Link to="/">
                 <img src={logo} alt="Beach Resort logo" />
               </Link>
-              <button type='button' className='nav-btn'>
+              <button 
+                type='button' 
+                className='nav-btn'
+                onClick={this.handleToggle}
+              >
                 <FaAlignRight className='nav-icon' />
               </button>
             </div>
