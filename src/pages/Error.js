d4401e07f9f700cd6b1fa6b54ed1de9d9b4b0388
Teresa Cho <tcho@cca.edu>
2020-07-03 21:53:50 -0700

    creates a Banner w/ 404 error code

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 
+
 export default function Error() {
-  return <Hero />
+  return <Hero>
+    <Banner title='404 Error' subtitle='no page exists'>
+
+    </Banner>
+  </Hero>
 };
