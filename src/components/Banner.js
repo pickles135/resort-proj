53e3ef325b383d8797e26af941bdb656926f47df
Teresa Cho <tcho@cca.edu>
2020-07-03 21:27:25 -0700

    creates a p tag and passes it subtitle prop and children prop down below

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,9 @@ export default function Banner({ children, title, subtitle }) {
   return (
     <div className="banner">
       <h1>{title}</h1>
+      <div></div>
+      <p>{subtitle}</p>
+      {children}
     </div>
   )
 };
