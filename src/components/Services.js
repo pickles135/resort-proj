61c73f20c99c450b0d5f9e96a137a73cca0e2c31
Teresa Cho <tcho@cca.edu>
2020-07-06 23:14:39 -0700

    creates state for each of the services, completes one.

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,6 +3,15 @@ import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
 import Title from './Title';
 
 export default class Services extends Component {
+  state={
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: 'free cocktails',
+        info: 'Lorem ipsum dolor sit amet, nonumes dissentiet per at.'
+      }
+    ]
+  }
   render() {
     return (
       <div>
