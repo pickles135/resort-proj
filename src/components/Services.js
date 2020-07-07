ddbfa33f67d007a38d8280210214f953170d65e6
Teresa Cho <tcho@cca.edu>
2020-07-06 22:27:29 -0700

    creates services.js component and adds some placeholder for class component

---
+++ src/components/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Services extends Component {
+  render() {
+    return (
+      <div>
+        hello from the services page
+      </div>
+    )
+  };
+};
