adfdde8247d92aece5ff2ccff8900bf28a95361b
Teresa Cho <tcho@cca.edu>
2020-07-06 22:49:06 -0700

    adds react fragment and renders the Services component right after Hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,16 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $29">
-        <Link to="/rooms" className='btn-primary'>
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $29">
+          <Link to="/rooms" className='btn-primary'>
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   )
 };
 
