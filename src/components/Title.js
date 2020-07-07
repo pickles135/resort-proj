bfd7e6e8898b287d01933fb35275bd946857517d
Teresa Cho <tcho@cca.edu>
2020-07-06 22:33:58 -0700

    creates an h4 tag with the title param inside

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title({ title }) {
   return (
     <div className="section-title">
-      This is the title
+      <h4>{title}</h4>
     </div>
   )
 }
